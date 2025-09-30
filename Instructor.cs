7bcca8becc8e47dfdf737d7f4eecf9bedbe350c5
yonesswaidan <115068530+yonesswaidan@users.noreply.github.com>
2025-09-30 14:23:02 +0200

    Delete

--- Instructor.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Instructor
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateTime HireDate { get; set; }
-}
-
