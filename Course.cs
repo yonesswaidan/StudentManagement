a22ea6466b3c4939871a1e4c4eb4629dfab441b7
yonesswaidan <115068530+yonesswaidan@users.noreply.github.com>
2025-09-30 14:21:15 +0200

    Delete

--- Course.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Course
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public int Credits { get; set; } // senere decimal
-    public int? InstructorId { get; set; }
-    public Instructor Instructor { get; set; }
-}
