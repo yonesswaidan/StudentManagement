e9d07e9de68014db0626412f6df715bb4b54f405
yonesswaidan <115068530+yonesswaidan@users.noreply.github.com>
2025-09-30 14:21:55 +0200

    Delete

--- Student.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Student
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateTime EnrollmentDate { get; set; }
-    // senere: MiddleName, DateOfBirth
-}
